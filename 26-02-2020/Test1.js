ee568ead34c77c72b428636b94ef07b5a0bd143c
Anatsrisaputh <you@example.com>
2020-02-26 22:55:14 +0700

    repeat javascript

---
+++ 26-02-2020/Test1.js
@@ -0,0 +1,13 @@
+let x = prompt("Who are you");
+    if (x === Admin) {
+        let y = prompt("Please enter your password");
+        if (y == "" || y == null) {
+            alert("Wellcome");
+        }
+    }
+
+
+
+
+
+
